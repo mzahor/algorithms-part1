17d2c8dba0c0bfac2d09b27ce9ddb0dd6da6347c
Marian Zagoruiko <marian.zagoruiko@gmail.com>
2014-02-03 22:22:38 +0200

    implements quick find

--- src/main/java/UnionFind.java
+++ src/main/java/UnionFind.java
@@ -1,4 +1,30 @@
+// This class implements Union-Find algorithm.
 
 public class UnionFind {
-    private
+    private int[] data;
+
+
+    public UnionFind(int n) {
+        data = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            data[i] = i;
+        }
+    }
+
+    public void union(int a, int b) {
+        if (data[a] == data[b]) return;
+        int prev = data[a];
+        int curr = data[b];
+
+        for(int i = 0; i < data.length; i++) {
+            if (data[i] == prev) {
+                data[i] = curr;
+            }
+        }
+    }
+
+    public boolean connected(int a, int b) {
+        return data[a] == data[b];
+    }
 }
